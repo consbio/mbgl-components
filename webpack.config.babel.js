d11825af28e8c4580a3d6c130ce4a3d1a0b208e2
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2020-03-13 15:01:59 -0700

    Add code and build config

---
+++ webpack.config.babel.js
@@ -0,0 +1,35 @@
+import path from 'path'
+
+export default {
+    mode: 'production',
+    devtool: 'source-map',
+    entry: './lib/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: path.resolve(__dirname, 'node_modules'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    output: {
+        filename: 'mbgl-components.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'mbgl-components'
+    },
+    resolve: {
+        modules: [
+            path.resolve(__dirname, 'node_modules')
+        ]
+    },
+    externals: ['mapbox-gl', 'react', 'react-dom', 'uuid'],
+    optimization: {
+        minimize: false
+    }
+}
